216d8647fc189bd6c54c5a2bde4e1487ea6d6c43
Joel Worrall <jworrall@newrelic.com>
2020-03-29 20:45:58 -0400

    fix: eslint fixes.

--- nerdlets/event-stream/index.js
+++ nerdlets/event-stream/index.js
@@ -5,11 +5,7 @@ export default class Root extends React.Component {
   render() {
     return (
       <NerdletStateContext.Consumer>
-        {nerdletUrlState => (
-              <EventStream
-                nerdletUrlState={nerdletUrlState}
-              />
-        )}
+        {nerdletUrlState => <EventStream nerdletUrlState={nerdletUrlState} />}
       </NerdletStateContext.Consumer>
     );
   }
